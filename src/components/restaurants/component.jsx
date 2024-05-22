053801f20ad41715a36cd451bf92f904276ee1e8
Olga Latkina <olga.latkina@gmail.com>
2024-05-23 06:34:04 +0700

    feat: add conditional rendering

--- src/components/restaurants/component.jsx
+++ src/components/restaurants/component.jsx
@@ -2,6 +2,14 @@
 import { Restaurant } from '../restaurant/component'
 
 export const Restaurants = ({ restaurants }) => {
+  if (!restaurants) {
+    return (
+      <main>
+        <h1>Oops!</h1>
+      </main>
+    )
+  }
+
   return (
     <main>
       <h1>Restaurants</h1>
