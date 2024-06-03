3d37900cc7644205d200ac75328a992c96aefcb4
Olga Latkina <olga.latkina@gmail.com>
2024-06-03 22:26:06 +0700

    feat: add a dish counter for an authorized user

--- src/components/dish/component.jsx
+++ src/components/dish/component.jsx
@@ -1,18 +1,26 @@
 import { Counter } from '../counter/component'
 import { useCount } from '../../hooks/use-count';
 
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/user';
+
 export const Dish = ({ name, price }) => {
   const { count, increment, decrement } = useCount()
+  const user = useContext(UserContext)
 
   return (
     <div>
       <p>{name}</p>
-      <Counter
-        value={count}
-        increment={increment}
-        decrement={decrement}
-      />
-      <p>Price: {count * price}</p>
+      {!!user && (
+        <>
+          <Counter
+            value={count}
+            increment={increment}
+            decrement={decrement}
+          />
+          <p>Price: {count * price}</p>
+        </>
+      )}
     </div>
   )
 }
